1a106e3d282e43e46413d45a230ee0b55bc8292f
Chandan Krishnani <chandan.krishnani@chicmic.co.in>
2023-07-21 17:28:01 +0530

    feature(swiftLint, pre-commitHook): add swiftLint and pre-commit hook to project

--- custom-media-playerTests/CustomMediaPlayerTests.swift
+++ custom-media-playerTests/CustomMediaPlayerTests.swift
@@ -8,10 +8,12 @@
 import XCTest
 @testable import custom_media_player
 
-final class custom_media_playerTests: XCTestCase {
+final class CustomMediaPlayerTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+//        var first_name: String = ""
+//        print(first_name)
     }
 
     override func tearDownWithError() throws {
@@ -23,7 +25,8 @@ final class custom_media_playerTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        // Mark your test async to allow awaiting for asynchronous code to complete. fdjlskfjdkslfjdsklfjdsklfjdsklfjdslfkdjskldsjfdklsfjdsklfjdsklfjds
+        // Check the results with assertions afterwards.
     }
 
     func testPerformanceExample() throws {
